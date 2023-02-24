09178fbf0d1d2c7225b8f9a97d448c495a0fefab
Meghna <Meghna@LAPTOP-SI22S4K0>
2023-02-24 21:11:06 +0530

    Created doSignup of model User

--- src/java/com/hiresphere/models/User.java
+++ src/java/com/hiresphere/models/User.java
@@ -4,10 +4,98 @@
  */
 package com.hiresphere.models;
 
+import com.hiresphere.services.UserService;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Map;
+import org.apache.log4j.Logger;
+import org.apache.struts2.dispatcher.ApplicationMap;
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.ApplicationAware;
+import org.apache.struts2.interceptor.SessionAware;
+
 /**
  *
  * @author user
  */
-public class User {
-    
+public class User extends ActionSupport implements ApplicationAware, SessionAware, Serializable {
+
+    private String name;
+    private String email;
+    private String password;
+
+    private SessionMap<String, Object> sessionMap = (SessionMap) ActionContext.getContext().getSession();
+
+    private ApplicationMap map = (ApplicationMap) ActionContext.getContext().getApplication();
+
+    @Override
+    public void setApplication(Map<String, Object> application) {
+        map = (ApplicationMap) application;
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        sessionMap = (SessionMap) session;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String doCandidateSignUp() {
+        String result = "FAILURE";
+
+        boolean success = UserService.doSignup(this);
+
+        if (success) {
+            System.out.println("returning Success from doSignup method");
+            result = "SUCCESS";
+
+        } else {
+            System.out.println("returning Failure from doSignup method");
+        }
+
+        return result;
+    }
+
+//    public String doLogin() throws Exception {
+//        String result = "FAILURE";
+//
+//        boolean success = LoginService.getInstance().doLogin(this);
+//
+//        if (success) {
+//            System.out.println("returning Success from doLogin method");
+//            result = "SUCCESS";
+//            sessionMap.put("Loggedin", this);
+//            ArrayList empList = EmployeeService.getInstance().getAllEmployees();
+//            
+//
+//        } else {
+//            System.out.println("returning Failure from doLogin method");
+//        }
+//
+//        return result;
+//    }
 }
--- src/java/com/hiresphere/services/UserService.java
+++ src/java/com/hiresphere/services/UserService.java
@@ -4,10 +4,50 @@
  */
 package com.hiresphere.services;
 
+import com.exavalu.utils.JDBCConnectionManager;
+import com.hiresphere.models.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author user
  */
 public class UserService {
-    
+
+    public static Object getInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public static boolean doSignup(User user) {
+        boolean result = false;
+        Connection con = JDBCConnectionManager.getConnection();
+
+        String sql = "INSERT INTO users(email,password,roleId,name)"
+                + "VALUES(? ,? ,? ,? ,? );";
+
+        try {
+
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, user.getEmail());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, 1);
+            preparedStatement.setString(1, user.getName());
+
+            int rs = preparedStatement.executeUpdate();
+
+            if (rs != 0) {
+                result = true;
+            }
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+
+        }
+
+        return result;
+    }
+
 }
